722969595ccfdd40a27aeb858c09afe3790c56f8
Jongseo-Lee <65481221+Jongseo-Lee@users.noreply.github.com>
2021-11-02 07:50:22 +0900

    practice

--- PCA.R
+++ PCA.R
@@ -22,23 +22,23 @@ factor_A16_2$loadings
 
 
 df_A17 <- df %>% 
-  select(A17_1:A17_15)
+  select(A17_1:A17_10)
 head(df_A17)
 
 pc2 <- prcomp(df_A17)
 summary(pc2)
-pc2$rotation
-pc2$sdev^2
-
+round(pc2$rotation,3)
+round(pc2$sdev^2,5)
+windows()
 screeplot(pc2, type = "l")
 
-factor_A17_1 <- factanal(df_A17, factors = 3, rotation = "varimax")
+factor_A17_1 <- factanal(df_A17, factors = 4, rotation = "varimax")
 factor_A17_1$loadings
-factor_A17_2 <- factanal(df_A17, factors = 3, rotation = "oblimin")
+factor_A17_2 <- factanal(df_A17, factors = 4, rotation = "oblimin")
 factor_A17_2$loadings
 
 df_A17_2 <- df %>% 
-  select(A17_8:A17_15)
+  select(A17_7:A17_16)
 head(df_A17_2)
 
 pc2_1 <- prcomp(df_A17_2)
@@ -48,7 +48,7 @@ pc2_1$sdev^2
 
 screeplot(pc2_1, type = "l")
 windows()
-factor_A17_3 <- factanal(df_A17_2, factors = 3, rotation = "varimax")
+factor_A17_3 <- factanal(df_A17_2, factors = 4, rotation = "varimax")
 factor_A17_3$loadings
 factor_A17_4 <- factanal(df_A17_2, factors = 3, rotation = "oblimin")
 factor_A17_4$loadings
